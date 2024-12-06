4df8b36d4514d17fd6d360aa186139c037f7fcc9
austineblaise <austineblaise@gmail.com>
2024-12-06 13:17:09 +0100

    configured app to

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+export default {
+    async headers() {
+      return [
+        {
+          source: "/(.*)", // Match all routes
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "default-src 'self' http: https: data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval';",
+            },
+            {
+              key: "Strict-Transport-Security",
+              value: "", // Disable HSTS to allow HTTP
+            },
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "*", // Allow all origins
+            },
+          ],
+        },
+      ];
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/api/:path*", // Proxy API requests
+          destination: "http://13.60.208.160/:path*", // Your HTTP API endpoint
+        },
+      ];
+    },
+  };
+  
+  
 
-export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
