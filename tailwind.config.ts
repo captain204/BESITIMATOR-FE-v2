439e7b41ceff870711ea4abfc3cdf87fa7a95e9b
austineblaise <austineblaise@gmail.com>
2024-12-31 00:38:22 +0100

    changed fontfamily again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = withMT({
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Montserrat"', "sans-serif"], // Set Century Gothic as the default sans-serif font
+        sans: ['"Century Gothic"', "sans-serif"], // Set Century Gothic as the default sans-serif font
       },
       colors: {
         primary: "#315aac",
